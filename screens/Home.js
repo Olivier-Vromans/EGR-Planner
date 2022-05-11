2eb76ca83288235e46b16c103dcc9ebe268109f9
Sander <sanderjverhoeven@hotmail.com>
2022-05-11 13:55:43 +0200

    added colors

--- screens/Home.js
+++ screens/Home.js
@@ -13,18 +13,27 @@ export default function Home({ navigation }) {
 
   return (
     <View style={styles.homeWrapper}>
-      {/* <Image source={leaf} style={styles.logoImage}/> */}
+      <StatusBar style="auto" />
       <Text style={styles.titleText}>EGR-Planner</Text>
-      <Text style={styles.inputFieldText}>Vertrekpunt</Text> 
+      {/* <Image source={leaf} style={styles.logoImage}/> */}
+      {/* <Text style={styles.inputFieldText}>Vertrekpunt</Text>  */}
      
       <GooglePlacesAutocomplete
-        placeholder='Location'
+        // Placeholder
+        placeholder='Vertrekpunt'
+        // Minimal text length before prediction
         minLength={1}
-        returnKeyType={'default'}
+        // what is the return
+        returnKeyType={'done'}
+        // fetch extra details
         fetchDetails={true}
+        // 	displays the result from autocomplete if the place details api return not found
         autoFillOnNotFound={true}
+        // hides "powered by Google" at the bottom of the search results list
         enablePoweredByContainer={false}
+        // removes all default styling from the library
         suppressDefaultStyles={true}
+        // the onPress handler
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
           onChangeOrigin(data.description)
@@ -35,15 +44,15 @@ export default function Home({ navigation }) {
         }}
         styles={{
           container:{
-            flex:0,
+            flex: 0,
           },
           textInputContainer: {
             height: 40,
             margin: 12,
             borderWidth: 1,
             padding: 10,
-            minWidth: 250,
-            maxWidth: 250,
+            minWidth: 350,
+            maxWidth: 350,
             borderRadius: 4,
             borderColor:'#ececec',
             alignSelf:'center',
@@ -56,7 +65,9 @@ export default function Home({ navigation }) {
             borderTopWidth: 0.5,
           },
           powered: {},
-          listView: {},
+          listView: {
+
+          },
           row: {
             backgroundColor: '#FFFFFF',
             padding: 13,
@@ -76,16 +87,24 @@ export default function Home({ navigation }) {
         }}
       />
       
-      <Text style={styles.inputFieldText}>Bestemming</Text>
+      {/* <Text style={styles.inputFieldText}>Bestemming</Text> */}
       
-      {/* 
       <GooglePlacesAutocomplete
-      placeholder='Location'
-      minLength={2}
-      autoFocus={true}
-      returnKeyType={'default'}
+      // Placeholder
+      placeholder='Bestemming'
+      // Minimal text length before prediction
+      minLength={1}
+      // what is the return
+      returnKeyType={'done'}
+      // fetch extra details
       fetchDetails={true}
+      // 	displays the result from autocomplete if the place details api return not found
       autoFillOnNotFound={true}
+      // hides "powered by Google" at the bottom of the search results list
+      enablePoweredByContainer={false}
+      // removes all default styling from the library
+      suppressDefaultStyles={true}
+      // the onPress handler
       onPress={(data, details = null) => {
         // 'details' is provided when fetchDetails = true
         onChangeDestination(data.description)
@@ -94,33 +113,76 @@ export default function Home({ navigation }) {
         key: 'AIzaSyADZtwlvQuxxtgjZ6YcSyDQdC7KKq0A3pY',
         language: 'nl',
       }}
-      styles={styles.input}
+      styles={{
+        container:{
+          flex: 0,
+        },
+        textInputContainer: {
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+          minWidth: 350,
+          maxWidth: 350,
+          borderRadius: 4,
+          borderColor:'#ececec',
+          alignSelf:'center',
+        },
+        poweredContainer: {
+          alignItems: 'center',
+          borderBottomRightRadius: 5,
+          borderBottomLeftRadius: 5,
+          borderColor: '#c8c7cc',
+          borderTopWidth: 0.5,
+        },
+        powered: {},
+        listView: {
+          alignSelf: 'center',
+          minWidth: 350,
+          maxWidth: 350,
+        },
+        row: {
+          backgroundColor: '#FFFFFF',
+          padding: 13,
+          height: 45,
+          flexDirection: 'row',
+        },
+        separator: {
+          height: 0.5,
+          backgroundColor: '#c8c7cc',
+        },
+        description: {},
+        loader: {
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          height: 20,
+        },
+      }}
     /> 
-    */}
 
-      <TextInput style={styles.textInputContainer} onChangeText={onChangeDestination} value={destination} placeholder={"Beurs"} />
+      {/* <TextInput style={styles.textInputContainer} onChangeText={onChangeDestination} value={destination} placeholder={"Beurs"} /> */}
 
-      <StatusBar style="auto" />
-
-      <Pressable style={styles.button} onPress={() => {
+      <Text style={styles.button} onPress={() => {
         //Check if origin and destination is not empty
         if (origin !== '' && destination !== '') {
-          /* Navigate to the Route route with params */
+          //Navigate to the Route route with params
           navigation.navigate('Map', {
             origin: origin,
             destination: destination
           })
         }
       }}>
-        <Text style={styles.textButton}>{">"}</Text>
-      </Pressable>
+        <Text style={styles.textButton}>Vertrek!</Text>
+      </Text>
 
-      <Button title="Go to the Map page" onPress={() => {
+      <Text style={styles.button} onPress={() => {
         navigation.navigate('Map', {
           origin: "zintele 4",
           destination: "Wijnhaven 99"
         })
-      }} />
+      }}>
+        <Text style={styles.textButton}>Go to the Map page</Text>
+      </Text>
     </View>
   );
 }
@@ -128,34 +190,49 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   homeWrapper: {
     backgroundColor: '#fff',
-    flex: 1,
-    alignItems: 'center',
+    alignContent: 'center',
+    height: '100%',
+    textAlign: 'center',
   },
 
   titleText:{
     fontSize: 42,
+    marginTop: 100,
+    marginBottom: 25,
+    textAlign:'center',
+    color: '#28D8A1',
   },
 
   inputFieldText: {
-    marginLeft: 12,
+    //textAlign:'center',
+    marginLeft: 35,
   },
 
   textInputContainer: {
+    alignSelf: 'center',
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    minWidth: 250,
+    minWidth: 350,
+    maxWidth: 350,
     borderRadius: 4,
-    borderColor:'#ececec',
+    borderColor: '#ececec',
   },
   button: {
     backgroundColor: '#28D8A1',
-    alignSelf: 'flex-end',
-    marginRight: 83,
-    paddingHorizontal: 12,
-    paddingVertical: 5,
+    textAlign: 'center',
+    alignSelf: 'center',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    minWidth: 350,
+    maxWidth: 350,
     borderRadius: 4,
+    borderColor: '#ececec',
+    textAlign: 'center',
+    textAlignVertical: 'center',
   },
   textButton: {
     color: '#fff',
